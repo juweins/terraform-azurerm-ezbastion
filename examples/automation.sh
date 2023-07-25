fe03024b2572d9ab1ce5c3d1b4429ee0444bca3d
juweins <26652109+juweins@users.noreply.github.com>
2023-07-25 14:23:32 +0200

    Added: Function to set terraform execution path for script

--- examples/automation.sh
+++ examples/automation.sh
@@ -2,13 +2,16 @@
 
 # This script is used to automate and run terraform apply/destroy
 #
-#   Usage: ./automation.sh [up|down|show]
+#   Usage: ./automation.sh [up|down|set|show]
 #   up:    Create Bastion
 #   down:  Destroy Bastion
+#   set:   Set Terraform directory path for execution
 #   show:  Display ezBastion configuration
 #
 
 # Set script variables
+CONFIG_FILE="$HOME/.ezBastion"
+
 bold=$(tput bold)
 normal=$(tput sgr0)
 
@@ -39,27 +42,57 @@ function configuration_show() {
     echo "${bold}Public IP        : ${normal}$TF_VAR_bastion_pip_name"
     echo "${bold}Bastion          : ${normal}$TF_VAR_bastion_name"
     echo "${bold}IP Configuration : ${normal}$TF_VAR_bastion_ip_configuration_name"
+    echo "${bold}Terraform Path   : ${normal}$(cat $CONFIG_FILE)"
     exit 1
+}
+
+# Set Terraform directory path to custom location
+function terraform_directory() {
+    echo "$1" >"$CONFIG_FILE"
+    echo "Terraform directory path set to: $1"
 }
 
 if [ "$1" = "up" ]; then
     echo "${bold}Creating Bastion... ${normal}"
+
+    # Load terraform directory path
+    terraform_dir=$(cat "$CONFIG_FILE")
+    # Check if the path is empty
+    if [ -z "$terraform_dir" ]; then
+        echo "Terraform directory path not set. Use './automate.sh set <terraform_directory>' to set it."
+        exit 1
+    fi
+    cd "$terraform_dir" || exit 1
+
+    # Execute Command
     terraform_up
+
 elif [ "$1" = "down" ]; then
     echo "${bold}Destroying Bastion... ${normal}"
+
+    # Load terraform directory path
+    terraform_dir=$(cat "$CONFIG_FILE")
+    # Check if the path is empty
+    if [ -z "$terraform_dir" ]; then
+        echo "Terraform directory path not set. Use './automate.sh set <terraform_directory>' to set it."
+        exit 1
+    fi
+    cd "$terraform_dir" || exit 1
+
+    # Execute Command
     terraform_down
 
 elif [ "$1" = "show" ]; then
     echo "${bold}ezBastion configuration"
     echo "-----------------------${normal}"
     configuration_show
+elif [ "$1" = "set" ]; then
+    echo "${bold}Setting Terraform directory... ${normal}"
+    terraform_directory $2
 else
-    echo "Usage: $0 [up|down|config]"
+    echo "Usage: $0 [up|down|set|show]"
     exit 1
 fi
-
-# cd to the directory containing the terraform files
-cd ..
 
 # Unset variables
 unset TF_VAR_bastion_resource_group_name
