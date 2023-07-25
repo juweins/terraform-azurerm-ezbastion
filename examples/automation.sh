17d9778317d766d32914032db8c65b62acf514d5
juweins <26652109+juweins@users.noreply.github.com>
2023-07-25 14:08:21 +0200

    Added: Options up/down/show to automation script

--- examples/automation.sh
+++ examples/automation.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
-# This script is used to automate and run terraform apply
+# This script is used to automate and run terraform apply/destroy
+#
+#   Usage: ./automation.sh [up|down|show]
+#   up:    Create Bastion
+#   down:  Destroy Bastion
+#   show:  Display ezBastion configuration
+#
 
-# Set variables
+# Set script variables
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+# Set Bastion specific variables
 export TF_VAR_bastion_rg_name="example-rg"
 export TF_VAR_bastion_vnet_name="example-vnet"
 export TF_VAR_bastion_subnet_name="AzureBastionSubnet"
@@ -10,23 +20,49 @@ export TF_VAR_bastion_pip_name="example-bastionPIP"
 export TF_VAR_bastion_name="autoBastion"
 export TF_VAR_bastion_ip_configuration_name="example-bastionIpConfig"
 
+# Create Bastion
+function terraform_up() {
+    terraform init
+    terraform apply --auto-approve
+}
 
-# Start of deployment
-echo "Running ezBastion automation script"
+# Destroy Bastion
+function terraform_down() {
+    terraform destroy --auto-approve
+}
+
+# Show Bastion configuration
+function terraform_show() {
+    echo "${bold}Resource Group   : ${normal}$TF_VAR_bastion_rg_name"
+    echo "${bold}VNet             : ${normal}$TF_VAR_bastion_vnet_name"
+    echo "${bold}Subnet           : ${normal}$TF_VAR_bastion_subnet_name"
+    echo "${bold}Public IP        : ${normal}$TF_VAR_bastion_pip_name"
+    echo "${bold}Bastion          : ${normal}$TF_VAR_bastion_name"
+    echo "${bold}IP Configuration : ${normal}$TF_VAR_bastion_ip_configuration_name"
+    exit 1
+}
+
+if [ "$1" = "up" ]; then
+    echo "${bold}Creating Bastion... ${normal}"
+    terraform_up
+elif [ "$1" = "down" ]; then
+    echo "${bold}Destroying Bastion... ${normal}"
+    terraform_down
+
+elif [ "$1" = "show" ]; then
+    echo "${bold}ezBastion configuration"
+    echo "-----------------------${normal}"
+    terraform_show
+else
+    echo "Usage: $0 [up|down|config]"
+    exit 1
+fi
 
 # cd to the directory containing the terraform files
 cd ..
-
-# Run terraform commands
-terraform init
-terraform apply -auto-approve
-
-# Finish deployment
-echo "Finished ezBastion automation script"
 
 # Unset variables
 unset TF_VAR_bastion_resource_group_name
 unset TF_VAR_bastion_vnet_name
 unset TF_VAR_bastion_subnet_name
 unset TF_VAR_bastion_pip_name
-
